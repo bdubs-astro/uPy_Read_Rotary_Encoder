8a743e261ef3284ce0db9a11be909b5fc6b6849c
bdubs-astro <bwissman@k-space.com>
2022-05-27 19:07:37 -0400

    Refactor display functions.

--- rotary_encoder_test.py
+++ rotary_encoder_test.py
@@ -2,38 +2,34 @@
 Tests a class used to read a KY-040 rotary encoder using interrupts.
 
 Resources:
+
+rotary encoder:
 https://github.com/gurgleapps/rotary-encoder
 https://gurgleapps.com/learn/electronics/ky-040-rotary-encoder-on-a-raspberry-pi-pico-detailed-explanation-and-step-by-step-code
-'''
 
-from rotary_bdw import Rotary
-import utime as time
-from machine import Pin, I2C
-from ssd1306 import SSD1306_I2C
-import framebuf
-
-rotary = Rotary(16,17,18)  # pin numbers: clk,dt,sw
-val = 0
-
-# I2C OLED setup
-from micropython import const
-oledSDA = const(4)
-oledSCL = const(5)
-WIDTH  = 128                                          
-HEIGHT = 32    
-i2c = I2C(0, scl=Pin(oledSCL), sda=Pin(oledSDA), freq=400000)
-oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, 0x3C)
-
-'''
-enable larger fonts for the OLED display
+larger fonts for the OLED display:
 https://github.com/peterhinch/micropython-font-to-py
 https://www.youtube.com/watch?v=bLXMVTTPFMs
 https://blog.miguelgrinberg.com/post/micropython-and-the-internet-of-things-part-vi-working-with-a-screen
 '''
-import writer
-import freesans20
 
-def dispStr (x, y, str, dispLogo = True):
+from rotary_bdw import Rotary
+import utime as time
+from micropython import const
+
+def disp_setup(scl_pin, sda_pin, width, height, id = 0, addr = 0x3C):
+    from machine import Pin, I2C
+    from ssd1306 import SSD1306_I2C
+    
+    i2c = I2C(id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
+    oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, addr)
+    return oled
+
+def disp_string (oled, x, y, str, disp_logo = True):
+    import framebuf
+    import writer
+    import freesans20
+
     # Raspberry Pi logo as 32x32 bytearray
     logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
      
@@ -41,7 +37,7 @@ def dispStr (x, y, str, dispLogo = True):
     font_writer = writer.Writer(oled, freesans20, False) # disable verbose mode
     font_writer.set_textpos(x, y)
     font_writer.printstring(str)
-    if dispLogo:
+    if disp_logo:
         fb = framebuf.FrameBuffer(logo, 32, 32, framebuf.MONO_HLSB)  # load the framebuffer
         # MONO_HLSB: monochrome, horizontally mapped - each byte occupies 8 horizontal pixels with bit 0 being the leftmost 
         oled.blit(fb, 96, 0) # blit the image from the framebuffer to the display
@@ -63,9 +59,20 @@ def rotary_changed(event):
         print(val)
             
     # display results on OLED ...
-    dispStr (5, 5, ('%d' %(val)))
-    
+    disp_string (oled, 5, 5, ('%d' %(val)))
+
+
+# rotary endcoder setup
+rotary = Rotary(16,17,18)  # pin numbers: clk,dt,sw
 rotary.add_handler(rotary_changed)
+val = 0
+
+# I2C OLED setup
+scl_pin= const(5)
+sda_pin = const(4)
+WIDTH  = 128                                          
+HEIGHT = 32  
+oled = disp_setup(scl_pin, sda_pin, WIDTH, HEIGHT)
 
 while True:
     time.sleep_ms(10)
